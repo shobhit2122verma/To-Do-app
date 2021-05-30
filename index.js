299671a37c3af47eca294229bebd866035ba158b
shobhit2122verma <44787115+shobhit2122verma@users.noreply.github.com>
2021-05-31 05:03:44 +0530

    successfully added the database and stored some data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const db=require('./config/mongoose.js');
+const todolist=require('./models/todolist');
 const express=require('express');//step1
 const port=8080;//step2
 const path=require('path');
@@ -9,8 +10,18 @@ app.set('view engine','ejs');//step 5 first we tell about the view engine that w
 app.set('views',path.join(__dirname,'views'));//step 6 here we join the path where we are setting up our view engine
 app.use(express.urlencoded());
 app.post('/addtask',function(req,res){
-    taskList.push(req.body.task);
-    return res.redirect('back');
+    todolist.create({
+        task:req.body.task,
+        category:req.body.category,
+        duedate:req.body.duedate
+    },function(err,newlist){
+        if(err){
+            console.log('error adding the schedule');
+            return;
+        }
+        console.log('added schedule' ,newlist);
+        return res.redirect('back');
+    })
 });
 
 app.get('/deletetask',function(req,res){
@@ -24,10 +35,16 @@ app.get('/deletetask',function(req,res){
 });
 
 app.get('/',function(req,res){
-    return res.render('home',{
-        title:'To-do List',
-        tasklist:taskList
-    });
+    todolist.find({},function(err,todolist){
+        if(err){
+            conosole.log("error getting data from the database");
+            return;
+        }
+        return res.render('home',{
+            title:'To-Do-List',
+            tasklist:todolist
+        });
+    })
 });
 app.listen(port,function(err){
     if(err)
